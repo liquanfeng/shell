668780baf2050f0e3a00a88d34b668a3fdf626fa
liquanfeng <liquanfeng7@gmail.com>
2018-07-29 12:01:16 +0800

    add function of pipe

--- smsh/execute.c
+++ smsh/execute.c
@@ -12,20 +12,28 @@ int execute(char* argv[]){
     int pid;
     int child_info = -1;
     int fd = 0;
+    int count = 0;
+    char** cptr = argv;
+    while(*cptr++)
+        ++count;
     if(argv[0] == NULL)
         return 0;
-    if((pid = fork()) == -1)
+    if((pid = fork()) == -1) 
         perror("fork");
-    else if(pid == 0){
+    else if(pid == 0){ 
         signal(SIGINT, SIG_DFL);
         signal(SIGQUIT, SIG_DFL);
         //execvp(argv[0], argv);
         if(strcmp(argv[0], "pwd")==0){
-            redirect(argv, &fd);
+            if(count>2 && strcmp(argv[1], ">")==0)
+                redirect(argv, &fd);
+            else if(count>2 && strcmp(argv[1], "|")==0)
+                dopipe(argv);
             execv("../pwd/pwd", NULL);
         }
         else if(strcmp(argv[0], "ls")==0){//bug
-            redirect(argv, &fd);
+            if(count>2 && strcmp(argv[1], ">")==0)
+                redirect(argv, &fd);
             execv("../ls/ls", NULL);
         }
         else if(strcmp(argv[0], "cp")==0){
@@ -54,16 +62,34 @@ int execute(char* argv[]){
 }
 
 void redirect(char** argv, int* fd){
-    int count = 0;
-    char** cptr = argv;
-    while(*cptr++)
-        ++count;
-    if(count==3 && strcmp(argv[1], ">")==0){
-        close(1);
-        if((*fd = creat(argv[2], COPYMODE))==-1){
-            fprintf(stderr, "Error: write error to ");
-            perror(argv[2]);
-            exit(1);
-        }
+    close(1);
+    if((*fd = creat(argv[2], COPYMODE))==-1)
+        fprintf(stderr, "Error: write error to ", argv[2]);
+}
+
+void dopipe(char** argv){
+    int thepipe[2], newfd, pid;
+    if(pipe(thepipe)==-1)
+        infobug("Error: Cannot get a pipe");
+    if((pid=fork())==-1)
+        infobug("Error: Cannot fork");
+    if(pid == 0){
+        close(thepipe[0]);
+        if(dup2(thepipe[1],1)==-1)
+            infobug("Error: Cannot redirect stdout");
+        close(thepipe[1]);
+        execlp(argv[0], argv[0], NULL);
+        infobug(argv[0]);
     }
+    close(thepipe[1]);
+    if(dup2(thepipe[0], 0)==-1)
+        infobug("Error: Cannot redirect stdin");
+    close(thepipe[0]);
+    execlp(argv[2], argv[2], NULL);
+    infobug(argv[2]);
+}
+
+void infobug(char *s){
+    fprintf(stderr, "Error: %s\n", s);
+    exit(1);
 }
